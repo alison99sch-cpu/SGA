bbdd24a8c24f377ff9050294b5cf562751704045
Alison <schalison77@gmail.com>
2026-08-27 15:56:30 -0300

    Clase 13 - Trabajamos con request y petición get

---
+++ backend/server.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const app = express()
+
+app.listen(3000, () => {
+    console.log("Servidor escuchando en http://localhost:3000")
+})
+
+const alumnos = [{
+    id: 1,
+    nombre: "Ali",
+    carrera: "Programación"
+    },
+    {  
+    id: 2,
+    nombre: "Rosa",
+    carrera: "Programación"
+
+    }
+
+]
+app.get("/alumnos", (req, res) => {
+    res.json(alumnos)
+} )
+
+app.get("/alumnos/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const alumno = alumnos.find(a => a.id ===id)
+    res.json(alumno)
+})
+
+
+const maestros = [{
+    id: 1,
+    nombre: "Sandra",
+    especialidad: "Historia"
+},
+{
+    id: 2,
+    nombre: "Horacio",
+    especialidad: "Matemáticas"
+},
+{
+    id: 3,
+    nombre: "Luisa",
+    especialidad: "Programación"
+},
+{
+    id: 4,
+    nombre: "Julian",
+    especialidad: "Legislación"
+},
+{
+    id: 5,
+    nombre: "Pedro",
+    especialidad: "Lengua y literatura"
+}
+
+]
+
+app.get("/maestros", (req, res) => {
+    res.JSON(maestros)
+})
+
+app.get("/maestros/:id", (req, res) => {
+const id = Number(req.params.id)
+const maestro = maestros.find(b => b.id === id)
+res.json(maestro)
+}) 
+
+
+
+
